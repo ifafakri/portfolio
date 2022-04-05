c9683395049f8ee4386c5d59d4ec83b52d4e1fbb
ifa fakhri <ifafakri@gmail.com>
2022-04-05 02:07:15 +0100

    template

---
+++ index.js
@@ -0,0 +1,51 @@
+const express=require('express')
+const app=express()
+app.use(express.static('public'))
+app.use(express.static('./node_modules'))
+app.use(express.static('./views/style'))
+app.use(express.static('./node_modules/font-awesome'))
+const handlebars=require('express-handlebars')
+
+
+port=3000
+
+
+
+app.set('port',3000)
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+
+app.get('/home',(req,res)=>{
+
+
+ res.render('home')
+
+
+})
+
+
+
+// 404 error page
+
+
+app.use(
+
+(req,res)=>{
+res.status(404)
+res.type('html')
+
+
+res.render('404')
+
+
+}
+
+)
+
+
+
+
+app.listen(app.get('port'))
+
